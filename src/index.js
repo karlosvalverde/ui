2f990d9f252b680e2b88acd507f2259edc02f4e8
Carlos Valverde <studio@carlos-valverde.com>
2019-12-16 21:28:12 +0100

    Version 1.0.1 // Fix jQuery bug

--- src/index.js
+++ src/index.js
@@ -8,31 +8,31 @@ require("bootstrap/dist/css/bootstrap.css");
 require("bootstrap-table");
 require("bootstrap-table/dist/bootstrap-table.min.css");
 require("@fortawesome/fontawesome-free/css/all.css");
-require("./styles.css");
+require("./styles.scss");
 
 $(document).ready(function() {
-      console.log('hello');
-      let counter = 0;
-      $(".pop").popover({
-        animation: true,
-        html: true,
-        content: function(){
-          return $('#popover_content_wrapper').html();
-       },
-        placement: "left"
-      });
-      $("#c-03").click(function(){
-        if (counter === 0){
-          $("#c-04").removeClass("far fa-plus-square");
-          $("#c-04").addClass("far fa-minus-square");
-          counter += 1;
-          console.log(counter);
-        }
-        else if (counter === 1){
-          $("#c-04").removeClass("far fa-minus-square");
-          $("#c-04").addClass("far fa-plus-square");
-          counter -= 1;
-          console.log(counter);
-        }  
-      });
+  console.log('hello');
+  let counter = 0;
+  $(".pop").popover({
+    animation: true,
+    html: true,
+    content: function(){
+      return $('#popover_content_wrapper').html();
+    },
+    placement: "left"
+  });
+  $("#c-03").click(function(){
+    if (counter === 0){
+      $("#c-04").removeClass("far fa-plus-square");
+      $("#c-04").addClass("far fa-minus-square");
+      counter += 1;
+      console.log(counter);
+    }
+    else if (counter === 1){
+      $("#c-04").removeClass("far fa-minus-square");
+      $("#c-04").addClass("far fa-plus-square");
+      counter -= 1;
+      console.log(counter);
+    }  
+  });
 });
